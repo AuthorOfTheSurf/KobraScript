92ab6383a67bd99d2f9d6746a0219ee86a7a1a5a
Edward Bramanti <edward@bramanti.org>
2014-02-24 18:16:12 -0800

    Added scanner testing program

---
+++ tests/test-scanner.js
@@ -0,0 +1,8 @@
+var should = require('should');
+var scan = require('../scanner')
+var error = require('../error')
+var i = require('util').inspect
+
+describe('The scanner', function () {
+  //  Where the tests go.
+})
