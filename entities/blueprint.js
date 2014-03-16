b11e72bb45da28af48a299fd98fc427ea6fda019
Edward Bramanti <edward@bramanti.org>
2014-03-16 15:42:11 -0700

    Renamed blueprint entity, beginning implementation.

---
+++ entities/blueprint.js
@@ -0,0 +1,55 @@
+var initialContext = require('../analyzer').initialContext
+var HashMap = require('hashmap').HashMap
+
+function Blueprint(block) {
+  this.block = block
+}
+
+Blueprint.prototype.toString = function () {
+  return '(Blueprint ' + this.block + ')' 
+}
+
+Blueprint.prototype.analyze = function () {
+  this.block.analyze(initialContext())
+}
+
+Blueprint.prototype.optimize = function () {
+  console.log('Optimization is not yet implemented')
+  return this
+}
+
+Blueprint.prototype.showSemanticGraph = function () {
+  /*var tag = 0
+  var seenEntities = new HashMap();
+
+  function dump(e, tag) {
+    var props = {}
+    for (var p in e) {
+      var value = rep(e[p])
+      if (value !== undefined) props[p] = value
+    }
+    console.log("%d %s %j", tag, e.constructor.name, props)
+  }
+
+  function rep(e) {
+    if (/undefined|function/.test(typeof e)) {
+      return undefined
+    } else if (/number|string|boolean/.test(typeof e)) {
+      return e
+    } else if (Array.isArray(e)) {
+      return e.map(rep)
+    } else if (e.kind) {
+      return e.lexeme
+    } else {
+      if (!seenEntities.has(e)) {
+        seenEntities.set(e, ++tag)
+        dump(e, tag)
+      }
+      return seenEntities.get(e)
+    }
+  }
+
+  dump(this, 0)*/
+}
+
+module.exports = Blueprint
