9dd420d543990878cab79a8f1be521c13d2fe413
AuthorOfTheSurf <zanekansil@gmail.com>
2014-03-16 17:11:19 -0700

    variablereference.js modified for dereferencing

--- entities/variablereference.js
+++ entities/variablereference.js
@@ -1,14 +1,22 @@
-function VariableReference(token) {
+function VariableReference(token, dereferences) {
   this.token = token
+  this.dereferences = dereferences
 }
 
 VariableReference.prototype.toString = function () {
-  return this.token.lexeme
+  var result = this.token.lexeme
+  if (dereferences.length > 0) {
+  	result = result.concat('.[')
+  	result = result.concat(dereferences.toString())
+  	result = result.concat(']')
+  }
+  return result
 }
 
 VariableReference.prototype.analyze = function (context) {
   this.referent = context.lookupVariable(this.token)
   this.type = this.referent.type
+  //make sure logic checks dereferences!
 }
 
 module.exports = VariableReference
