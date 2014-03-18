73a436fddfd1bfd399976ad4d1425f9f84dc6bf0
Edward Bramanti <edward@bramanti.org>
2014-03-17 21:39:42 -0700

    Fixed error in file extension check.

--- kobra.js
+++ kobra.js
@@ -6,7 +6,6 @@ var path = require('path')
 var argv = parseArgs(process.argv.slice(2), opts={
   boolean: ['t', 'a', 'o', 'i']
 })
-if (argv._.length === 0) console.log("This is KobraScript compiler!\n$ node kobra.js [-t] [-a] [-o] [-i] filename\n-t scans, prints the tokens, then exits\n-a scans, parses, prints the abstract syntax tree, then exits\n-o does optimizations\n-i goes up to semantic analysis, prints the semantic graph, then exits");
 var scan = require('./scanner')
 var parse = require('./parser')
 var generate = require('./generator')('js')
@@ -16,30 +15,35 @@ var error = require('./error')
 var fileExtension = path.extname(argv._[0])
 var validExtension = fileExtension === '.ks' || fileExtension === '.ksb'
 
-if (validExtension) {
-  scan(argv._[0], function (tokens) {
-    if (error.count > 0) return;
-    if (argv.t) {
-      tokens.forEach(function (t) {console.log(t)})
-      return
-    }
-    var program = parse(tokens)
-    if (error.count > 0) return;
-    if (argv.a) {
-      console.log(program.toString())
-      return
-    }
-    if (argv.o) {
-      program = program.optimize()
-    }
-    program.analyze()
-    if (error.count > 0) return;
-    if (argv.i) {
-      program.showSemanticGraph()
-      return
-    }
-    generate(program)
-  })
+//  Begin execution.
+if (argv._.length === 0) {
+  console.log("This is KobraScript compiler!\n$ node kobra.js [-t] [-a] [-o] [-i] filename\n-t scans, prints the tokens, then exits\n-a scans, parses, prints the abstract syntax tree, then exits\n-o does optimizations\n-i goes up to semantic analysis, prints the semantic graph, then exits");
 } else {
-  error('Invalid extension for ' + path.basename(argv._[0]) + ', expected .ks or .ksb', {path: fileExtension})
+  if (validExtension) {
+    scan(argv._[0], function (tokens) {
+      if (error.count > 0) return;
+      if (argv.t) {
+        tokens.forEach(function (t) {console.log(t)})
+        return
+      }
+      var program = parse(tokens)
+      if (error.count > 0) return;
+      if (argv.a) {
+        console.log(program.toString())
+        return
+      }
+      if (argv.o) {
+        program = program.optimize()
+      }
+      program.analyze()
+      if (error.count > 0) return;
+      if (argv.i) {
+        program.showSemanticGraph()
+        return
+      }
+      generate(program)
+    })
+  } else {
+    error('Invalid extension for ' + path.basename(argv._[0]) + ', expected .ks or .ksb', {path: fileExtension})
+  }
 }
