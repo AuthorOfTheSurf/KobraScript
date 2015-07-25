fff299164a859513d1586e1630917bea7ceee003
AuthorOfTheSurf <zanekansil@gmail.com>
2015-07-24 21:14:30 -0700

    Update kobrac

--- kobrac.js
+++ kobrac.js
@@ -1,28 +1,30 @@
 #!/usr/bin/env node
-var parseArgs     = require('minimist')
-var path          = require('path')
-var argv          = parseArgs(process.argv.slice(2), opts={
-  boolean: ['t', 'a', 'o', 'i'],
-})
+var path = require('path')
 
 var scan          = require('./scanner')
 var parse         = require('./parser')
-var error         = require('./error')
+var parseArgs = require('minimist')
+var opts = {
+  boolean: ['t', 'a', 'o', 'i']
+}
+var argv = parseArgs(process.argv.slice(2), opts)
+var noArgs = argv._.length === 0
 
-var fileExtension = path.extname(argv._[0])
 var currentDir    = path.dirname(argv._[0])
+var error = require('./error')
 
-//  Begin execution.
-if (argv._.length === 0) {
-  console.log(
-    "This is KobraScript compiler!\n",
-    "$ kobrac [-t] [-a] [-o] [-i] filename\n",
-    "-t scans, prints the tokens, then exits\n",
-    "-a scans, parses, prints the abstract syntax tree, then exits\n",
-    "-o does optimizations\n",
-    "-i goes up to semantic analysis, prints the semantic graph, then exits\n");
+if (noArgs) {
+  console.log([
+    'This is KobraScript compiler!',
+    '$ kobrac [-t] [-a] [-o] [-i] filename',
+    '-t scans, prints the tokens, then exits',
+    '-a scans, parses, prints the abstract syntax tree, then exits',
+    '-o does optimizations',
+    '-i goes up to semantic analysis, prints the semantic graph, then exits'
+  ].join('\n'));
 } else {
-  // Check for proper .ks extension if arg provided
+  var fileExtension = path.extname(argv._[0])
+  
   if ('.ks'.indexOf(fileExtension) < 0) {
     error('Invalid extension for ' + path.basename(argv._[0]) + ', expected .ks', { path: fileExtension })
     return
